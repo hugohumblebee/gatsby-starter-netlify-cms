2b7170603891142c832d69d071dfb9ffdc1479a9
hugo humblebee <hugo.hedelin@humblebee.se>
2020-05-16 11:15:14 +0200

    Trying to update the CMS options

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,127 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { navigate } from "gatsby-link";
 import Layout from "../../components/Layout";
-import firebase from "gatsby-plugin-firebase";
-import "firebase/database";
-import { useObjectVal } from "react-firebase-hooks/database";
+import { firestore, useFirestoreQuery } from "gatsby-theme-firebase";
 
-function encode(data) {
-  return Object.keys(data)
-    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    .join("&");
-}
+// import firebase from "gatsby-plugin-firebase";
+// import "firebase/database";
+// import { useObjectVal } from "react-firebase-hooks/database";
+// import { Form, FormState } from "gatsby-theme-firebase";
 
-export default class Index extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { isValidated: false };
-  }
+const Index = (props) => {
+  const query = firestore.collection("DogRaces").orderBy("AvgWeight", "asc");
+  const [data, isLoading, error] = useFirestoreQuery(query);
 
-  componentDidMount() {
-    firebase
-      .database()
-      .ref("DogRaces")
-      .then((snapshot) => {
-        console.log(snapshot.val());
-      });
-  }
+  const [isValidated, setIsValidated] = useState(false);
 
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const form = e.target;
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({
-        "form-name": form.getAttribute("name"),
-        ...this.state,
-      }),
-    })
-      .then(() => navigate(form.getAttribute("action")))
-      .catch((error) => alert(error));
-  };
-
-  render() {
-    return (
-      <Layout>
-        <section className="section">
-          <div className="container">
-            <div className="content">
-              <h1>Contact</h1>
-              <form
-                name="contact"
-                method="post"
-                action="/contact/thanks/"
-                data-netlify="true"
-                data-netlify-honeypot="bot-field"
-                onSubmit={this.handleSubmit}
-              >
-                {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-                <input type="hidden" name="form-name" value="contact" />
-                <div hidden>
-                  <label>
-                    Don’t fill this out:{" "}
-                    <input name="bot-field" onChange={this.handleChange} />
-                  </label>
-                </div>
-                <div className="field">
-                  <label className="label" htmlFor={"name"}>
-                    Your name
-                  </label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type={"text"}
-                      name={"name"}
-                      onChange={this.handleChange}
-                      id={"name"}
-                      required={true}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <label className="label" htmlFor={"email"}>
-                    Email
-                  </label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type={"email"}
-                      name={"email"}
-                      onChange={this.handleChange}
-                      id={"email"}
-                      required={true}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <label className="label" htmlFor={"message"}>
-                    Message
-                  </label>
-                  <div className="control">
-                    <textarea
-                      className="textarea"
-                      name={"message"}
-                      onChange={this.handleChange}
-                      id={"message"}
-                      required={true}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <button className="button is-link" type="submit">
-                    Send
-                  </button>
-                </div>
-              </form>
-            </div>
+  return (
+    <Layout>
+      <section className="section">
+        <div className="container">
+          <div className="content">
+            {isLoading && "Loading.."}
+            {data && JSON.stringify(data)}
+            {error && error}
           </div>
-        </section>
-      </Layout>
-    );
-  }
-}
+        </div>
+      </section>
+    </Layout>
+  );
+};
+
+export default Index;
