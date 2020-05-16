6146cf28aba66df2445c84be0e6609ccf219b7a6
hugo humblebee <hugo.hedelin@humblebee.se>
2020-05-16 13:48:20 +0200

    Testing to add custom widget

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -16,3 +16,32 @@ CMS.registerPreviewTemplate("about", AboutPagePreview);
 CMS.registerPreviewTemplate("products", ProductPagePreview);
 CMS.registerPreviewTemplate("blog", BlogPostPreview);
 CMS.registerPreviewTemplate("customs", NewPagePreview);
+
+var CategoriesControl = createClass({
+  handleChange: function (e) {
+    this.props.onChange(e.target.value.split(",").map((e) => e.trim()));
+  },
+
+  render: function () {
+    var value = this.props.value;
+    return h("input", {
+      type: "text",
+      value: value ? value.join(", ") : "",
+      onChange: this.handleChange,
+    });
+  },
+});
+
+var CategoriesPreview = createClass({
+  render: function () {
+    return h(
+      "ul",
+      {},
+      this.props.value.map(function (val, index) {
+        return h("li", { key: index }, val);
+      })
+    );
+  },
+});
+
+CMS.registerWidget("categories", CategoriesControl, CategoriesPreview);
